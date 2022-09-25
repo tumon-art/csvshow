c39b9151e23c96bf71fd4137d09a8ed51bea3be1
tumon <tumonkhn@gmail.com>
2022-09-26 04:29:33 +0600

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,26 +4,47 @@ import React from "react";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  const [file, setFile] = React.useState<any | null>(null);
+  const [file, setFile] = React.useState<Blob>({} as Blob);
+  const [array, setArray] = React.useState<string[] | any>();
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFile(e.target.files![0]);
   };
 
+  const csvFileToArray = (string: string) => {
+    var csvHeader = string.slice(0, string.indexOf("\n")).split(",");
+    const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
+
+    const array = csvRows.map((i) => {
+      const values = i.split(",");
+      const obj = csvHeader.reduce((object: string | any, header, index) => {
+        object[header] = values[index];
+        return object;
+      }, {});
+      return obj;
+    });
+    setArray(array);
+  };
+
   const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const fileReader = new window.FileReader();
+
+    const fileReader = new FileReader();
+    fileReader.readAsText(file);
 
     if (file) {
-      fileReader.onload = function (event) {
-        const csvOutput = event.target!.result;
-        console.log(csvOutput);
+      fileReader.onload = (event: ProgressEvent<FileReader>) => {
+        const text: string | any = event.target?.result?.toString();
+        csvFileToArray(text);
       };
-
-      fileReader.readAsText(file);
     }
   };
 
+  let headerKeys: [] | any;
+
+  if (array) headerKeys = Object.keys(Object.assign({}, ...array));
+
+  console.log(Object.values(array[0]));
   return (
     <div>
       <Head>
@@ -42,6 +63,30 @@ const Home: NextPage = () => {
             onChange={handleOnChange}
           />
           <button type="submit">IMPORT CSV</button>
+
+          <br />
+
+          {headerKeys && (
+            <table>
+              <thead>
+                <tr key={"header"}>
+                  {headerKeys.map((key: string) => (
+                    <th>{key}</th>
+                  ))}
+                </tr>
+              </thead>
+
+              <tbody>
+                {array.map((item: {} | any) => (
+                  <tr key={item.id}>
+                    {Object.values(item).map((val: string | any) => (
+                      <td>{val}</td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
         </form>
       </div>
     </div>
